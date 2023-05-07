c47da6716c7a66a4dea3a8677a942421a067458d
AnaGarofalo <anamariagarofalo95@gmail.com>
2023-05-07 08:49:50 -0300

    created Products model

---
+++ api/src/models/Products.js
@@ -0,0 +1,44 @@
+const { DataTypes } = require("sequelize");
+
+//* Definiendo la función que crea el modelo Products
+module.exports = (sequelize) => {
+  sequelize.define(
+    "Products",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      price: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+      },
+      discount: {
+        type: DataTypes.FLOAT,
+        allowNull: true,
+      },
+      stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      state: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    { timestamps: false }
+  );
+};
