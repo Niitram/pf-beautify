9e3a271dc938742edbe1c47f0a5d185ef6104336
AnaGarofalo <anamariagarofalo95@gmail.com>
2023-05-07 09:59:35 -0300

    created Categorys model

---
+++ api/src/models/Categorys.js
@@ -0,0 +1,16 @@
+const { DataTypes } = require("sequelize");
+
+//* Definiendo la función que crea el modelo Categorys
+module.exports = (sequelize) => {
+  sequelize.define("Category", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+  });
+};
