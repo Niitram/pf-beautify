2c50361d630d94f97f4e02cd4d9c3526417b24b7
Niitram <65573729+Niitram@users.noreply.github.com>
2023-05-07 20:20:27 -0300

    Update useToggle.js
    I changed the order of the returned values so that it first returns the value and then the setter function
    
    Signed-off-by: Niitram <65573729+Niitram@users.noreply.github.com>

--- client/src/hooks/useToggle.js
+++ client/src/hooks/useToggle.js
@@ -1,14 +1,11 @@
 import { useState, useCallback } from "react";
 
-
-
 const useToggle = (initialToggleValue = false) => {
     const [value, setValue] = useState(initialToggleValue)
     const toggle = useCallback(() => {
         setValue(value => !value)
     }, [])
-    return [toggle, value]
+    return [value, toggle]
 }
-
 
 export default useToggle;
