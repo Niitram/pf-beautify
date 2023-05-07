90e8f75bd30dec3dbb798b72bdea96cdec666f55
AnaGarofalo <anamariagarofalo95@gmail.com>
2023-05-06 21:47:17 -0300

    create Clients model

---
+++ api/src/models/Clients.js
@@ -0,0 +1,38 @@
+const { DataTypes } = require("sequelize");
+
+//* Definiendo la función que crea el modelo Genre
+module.exports = (sequelize) => {
+  sequelize.define(
+    "Client",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      fullName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: { isEmail: true },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      adress: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      phone: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    { timestamps: false }
+  );
+};
