20f3ff63de905114bc52cf55396e2f779a3acd69
AnaGarofalo <anamariagarofalo95@gmail.com>
2023-05-08 10:30:52 -0300

    updated_associations

--- api/src/models/Comments.js
+++ api/src/models/Comments.js
@@ -2,20 +2,19 @@ const { DataTypes } = require("sequelize");
 
 //* Definiendo la función que crea el modelo Comments
 module.exports = (sequelize) => {
-    sequelize.define(
-        "Comment",
-        {
-            id: {
-                type: DataTypes.INTEGER,
-                primaryKey: true,
-                autoIncrement: true
-            },
-            content: {
-                type: DataTypes.STRING,
-                allowNull: false
-            },
-
-        },
-        { timestamps: false }
-    );
-}
+  sequelize.define(
+    "Comment",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      content: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    { timestamps: false }
+  );
+};
