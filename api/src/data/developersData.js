89c3b6d6b03d748e862056a216d70b57a5b24f85
AnaGarofalo <anamariagarofalo95@gmail.com>
2023-05-09 11:58:06 -0300

    added feature to data/developersData

--- api/src/data/developersData.js
+++ api/src/data/developersData.js
@@ -3,6 +3,10 @@ const developers = require("./developers.json");
 
 const bulkCreateDevelopers = async () => {
   try {
+    //* checking there are not products in the database
+    const oldDevelopers = await Developer.findAll();
+    if (oldDevelopers.length) return;
+
     await Developer.bulkCreate(developers);
     console.log("Se han cargado los developers a la base de datos");
   } catch (error) {
