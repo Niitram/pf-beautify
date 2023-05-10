b4e122a86def619ab4fa111478e914e38e85e4b7
lordeber15 <eberpalomino@hotmail.com>
2023-05-09 23:37:28 -0500

    Componente DetailProducts

--- client/src/views/detailProduct/DetailProduct.jsx
+++ client/src/views/detailProduct/DetailProduct.jsx
@@ -1,6 +1,5 @@
 import { Stack, Rating } from "@mui/material";
-import LinearProgress from "@mui/material/LinearProgress";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import styles from "./DetailProduct.module.css";
 
@@ -17,41 +16,43 @@ function DetailProduct() {
         <h1 className={styles.nombreProduct}>Nombre del producto</h1>
         <h3 className={styles.descripcionProduct}>DESCRIPCION DEL PRODUCTO</h3>
         <Stack>
-          <Rating defaultValue={2.5} precision={0.5} readOnly />
+          <Rating defaultValue={2.5} precision={0.5} />
         </Stack>
         <div className={styles.descripcionProduct}>
           <h3 className={styles.descuento}>$29.99</h3>
           <h3 className={styles.precio}> $19.99 </h3>
         </div>
-        <label className={styles.stock}>Quedan {cantStock} Productos</label>
-        <LinearProgress
-          sx={{ width: `100%`, height: 5, borderRadius: "16px" }}
-          variant="determinate"
-          value={80}
-        />
-        <progress max="100" value="80">
+
+        <label className={styles.stock}>Quedan {cantStock} Unidades</label>
+        <progress className={styles.progressBar} max="100" value="80">
           80
         </progress>
-        <label className={styles.descripcionDetailProduct}>Descripcion</label>
+        <label className={styles.descripcionDetailProduct}>Descripción</label>
         <p className={styles.descripction}>
           Ea ut sint nisi cillum anim non proident amet eiusmod non id id fugiat
           consequat. Laboris aute et pariatur Lorem. Nulla tempor nulla laboris
           elit.
         </p>
-        <label className={styles.more}>Leer mas</label>
-        <div>
+        {/* <label className={styles.more}>Leer mas</label> */}
+        <div className={styles.apartadoCompras}>
           <label className={styles.cantidad}>Cantidad</label>
-          <input type="number" min="1" defaultValue="1" />
+          <input
+            className={styles.inputCantidad}
+            type="number"
+            min="1"
+            max="5"
+            defaultValue="1"
+          />
           <label className={styles.shopMax}>Compra Máxima 5</label>
           <button className={styles.btnShopNow} type="submit">
             Comprar Ahora
           </button>
-          <div>
+          <div className={styles.btnCartAndList}>
             <button className={styles.addCart}>
-              <ShoppingCartIcon className={styles.icoCart} /> Agregar al Carrito
+              <ShoppingCartOutlinedIcon /> Agregar al Carrito
             </button>
             <button className={styles.listWish}>
-              <FavoriteBorderIcon className={styles.icoList} />
+              <FavoriteBorderIcon />
               Lista de Deseos
             </button>
           </div>
