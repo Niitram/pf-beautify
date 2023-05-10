5d5b64fe1ec38a26fe88e13d13d91b4797ec9541
AnaGarofalo <anamariagarofalo95@gmail.com>
2023-05-10 16:52:16 -0300

    added shops and clients automaticlly to database

--- api/index.js
+++ api/index.js
@@ -1,14 +1,18 @@
 const server = require("./src/app.js");
+const bulkCreateClients = require("./src/data/clients.js");
 const bulkCreateDevelopers = require("./src/data/developersData.js");
 const bulkCreateProducts = require("./src/data/productsData.js");
+const bulkCreateShops = require("./src/data/shops.js");
 const { conn } = require("./src/db.js");
 
 // Syncing all the models at once.
 
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
+  server.listen(3001, async () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
-    bulkCreateProducts();
-    bulkCreateDevelopers();
+    await bulkCreateProducts();
+    await bulkCreateDevelopers();
+    await bulkCreateClients();
+    await bulkCreateShops();
   });
 });
