27097128f07832ba05d97cc6e8bac26d7e3c9030
Facundovila03 <facundovila03@gmail.com>
2023-05-11 00:18:47 -0300

    fix landing css problems

--- client/src/views/landing/Landing.jsx
+++ client/src/views/landing/Landing.jsx
@@ -2,6 +2,7 @@ import logo from "../../assets/images/LandingImg.svg";
 // import { useState } from "react";
 import styles from "./Landing.module.css";
 // import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function Landing() {
   //   const [loginVisible, setLoginVisible] = useState(false);
@@ -11,20 +12,21 @@ export default function Landing() {
   //     setLoginVisible(!loginVisible);
   //   };
 
+  const navigate = useNavigate();
+
   return (
     <div className={styles.Container}>
       <div className={styles.EmbraceYourBeauty}>
         <h2>
           <strong>Embrace your</strong>
         </h2>
-        <h2 className={styles.beauty}>
-          <strong>beauty</strong>
-        </h2>
+        <h2 className={styles.beauty}>beauty</h2>
         <button
+          onClick={() => navigate("/home")}
           className={styles.Login}
           // onClick={handleLoginClick}
         >
-          <strong>Log in</strong>
+          Log in
         </button>
       </div>
       {/* <div
