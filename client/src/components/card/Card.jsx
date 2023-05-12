d681476fca2e42d72515a3355ef8a00e97bb6df4
martin marchessi <niitram.mm@gmail.com>
2023-05-12 13:44:08 -0300

    estilos de cards arreglados

--- client/src/components/card/Card.jsx
+++ client/src/components/card/Card.jsx
@@ -4,25 +4,22 @@ import { Link } from "react-router-dom";
 
 function Card({ image, price, name, rate, id }) {
   return (
-    <div className={styles.containerGlobal}>
-      <Link style={{ textDecoration: "none" }} to={`/detailProduct/${id}`}>
-        <div className={styles.cardContainer}>
-          <div className={styles.imagen}>
-            <img src={image} />
-          </div>
-          <div className={styles.detailsCard}>
-            <div className={styles.nombre}>{name}</div>
-            <Rating
-              name="size-small"
-              defaultValue={rate}
-              size="small"
-              readOnly
-            />
-            <div className={styles.precio}>${price}</div>
-          </div>
+    <Link
+      className={styles.link}
+      style={{ textDecoration: "none" }}
+      to={`/detailProduct/${id}`}
+    >
+      <div className={styles.cardContainer}>
+        <div className={styles.imagen}>
+          <img src={image} />
         </div>
-      </Link>
-    </div>
+        <div className={styles.detailsCard}>
+          <div className={styles.nombre}>{name}</div>
+          <Rating name="size-small" defaultValue={rate} size="small" readOnly />
+          <div className={styles.precio}>${price}</div>
+        </div>
+      </div>
+    </Link>
   );
 }
 
