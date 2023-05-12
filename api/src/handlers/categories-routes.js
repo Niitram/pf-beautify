c17ca51b7e7e977efc5bf8162d53e012fee23669
AnaGarofalo <anamariagarofalo95@gmail.com>
2023-05-12 15:48:08 -0300

    post route and validation

--- api/src/handlers/categories-routes.js
+++ api/src/handlers/categories-routes.js
@@ -1,13 +1,30 @@
 const router = require("express").Router();
 const getCategories = require("../controllers/Categories/getCategories");
-const  validateCategoriesExistence = require('../validations/validationCategories')
+const postCategory = require("../controllers/Categories/postCategory");
+const postCategoryValidation = require("../validations/postCategory");
+const validateCategoriesExistence = require("../validations/validationCategories");
+
 router.get("/", validateCategoriesExistence, async (req, res) => {
   try {
     const categories = await getCategories();
     res.json(categories);
   } catch (error) {
-    res.json({ error: error.message });
+    res.status(500).json({ error: error.message });
   }
 });
+
+router.post("/", postCategoryValidation, async (req, res) => {
+  try {
+    const { name } = req.body;
+    const newCategory = await postCategory(name);
+    res.status(201).json(newCategory);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.put("/:id");
+
+router.delete("*:id");
 
 module.exports = router;
---
+++ api/src/validations/postCategory.js
@@ -0,0 +1,19 @@
+const { Category } = require("../db");
+
+const postCategoryValidation = async (req, res, next) => {
+  const { name } = req.body;
+  if (!name || !name.length)
+    return res.status(400).json({ error: "name required" });
+
+  if (name.length > 255)
+    return res.status(400).json({ error: "name too long" });
+
+  const oldName = name[0].toUpperCase() + name.slice(1);
+
+  const oldCategory = await Category.findOne({ where: { name: oldName } });
+  if (oldCategory)
+    return res.status(400).json({ error: "category alredy exists" });
+  next();
+};
+
+module.exports = postCategoryValidation;
