4572f6b3ce7ec993f86dadeb6aa713e8bbd6d0f7
Facundovila03 <facundovila03@gmail.com>
2023-05-14 17:58:57 -0300

    deje home bien facherito

--- client/src/components/promo card/PromoCard.jsx
+++ client/src/components/promo card/PromoCard.jsx
@@ -1,10 +1,29 @@
+import { useState } from 'react'
+import {useNavigate} from 'react-router-dom'
 import styles from './PromoCard.module.css'
 
 export default function PromoCard ({image,id,name}){
+
+    const navigate = useNavigate()
+    const [hover,setHover]= useState(false)
+    const handleHover = ()=>{
+        setHover(!hover)
+    }
+
+    const handleClick = ()=>{
+        navigate(`/detailProduct/${id}`)
+    }
+
     return (
-        <div className={styles.Container}>
+        <div className={styles.Container} onMouseEnter={handleHover} onMouseLeave={handleHover}>
             <img src={image} alt={name}/>
-            {/* <p>{name}</p> */}
+            {hover && (
+                <div style={{position:'absolute',width:'100%',height:'100%',display:'flex',alignItems:'center',justifyContent:'center'}} onClick={handleClick}>
+                <div className={styles.overlay}/>
+                <h1 className={styles.noBlur}>{name.toUpperCase()}</h1>
+                </div>
+            )}
+           
         </div>
     )
 }
