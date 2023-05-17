2eb01d1bc20e2e73fb7a4c86a565340c00f0f5eb
AnaGarofalo <anamariagarofalo95@gmail.com>
2023-05-16 21:12:03 -0300

    clients by email

--- api/src/controllers/Clients/getClientById.js
+++
@@ -1,9 +0,0 @@
-const { Client } = require("../../db");
-
-
-const getClientById = async (id) => {
-    const client = await Client.findByPk(id)
-    return client.dataValues
-}
-
-module.exports = getClientById
