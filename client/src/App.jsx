88899c4644e1319b0059e527a00632655a3dd75d
Franco Rocamora <martin.rocamoraf@gmail.com>
2023-05-18 10:48:00 -0300

    test front

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css'
+import axios from 'axios'
 import { Route, Routes, useLocation } from "react-router-dom"
+import { useState } from 'react'
 import Landing from "./views/landing/Landing"
 import Home from "./views/home/Home"
 import About from "./views/about/About"
@@ -11,9 +13,28 @@ import DetailProduct from "./views/detailProduct/DetailProduct"
 import DetailPayment from "./views/detailPayment/DetailPayment"
 import DetailUser from "./views/detailUser/DetailUser"
 import Nav from './components/nav/Nav'
+import { initMercadoPago, Wallet } from '@mercadopago/sdk-react'
+initMercadoPago('TEST-6baebe46-f407-406f-8011-2f812f18a2a3');
 
 function App() {
   const locationNow = useLocation()
+
+  const [preferenceId, setPreferenceId] = useState(null);
+  const [orderData, setOrderData] = useState([{ quantity: 1, unit_price: 10, email: 'test@gmail.com', description: "Blotted Lip" },
+  { quantity: 1, unit_price: 10, email: 'test@gmail.com', description: "Eyeliner" }]);
+
+  const handleClick = () => {
+    
+    axios.post("http://localhost:3001/mercadopago/create_preference", orderData)
+    .then(response => {
+      console.log(response.id)
+      setPreferenceId(response.data.id)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  };
+  
 
 
   return (
@@ -21,19 +42,10 @@ function App() {
       {
         locationNow.pathname !== "/" && <Nav />
       }
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/dashboardAdmin" element={<DashboardAdmin />} />
-        <Route path="/detailUser" element={<DetailUser />} />
-        <Route path="/detailProduct" element={<DetailProduct />} />
-        <Route path="/detailPayment" element={<DetailPayment />} />
-      </Routes>
-    </>
+       <button id="wallet_container" onClick={handleClick}  ></button>
+     
+       <Wallet  initialization={{ preferenceId: `${preferenceId}` }}  />
+      </>
   )
 }
 
