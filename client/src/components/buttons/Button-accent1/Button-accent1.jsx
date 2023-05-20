754e4a377f06c1b308cd3e4976fbe233af0743fa
jesusudiz <jesusgabrieludiz@gmail.com>
2023-05-20 18:09:55 -0400

    retomando cambios

--- client/src/components/buttons/Button-accent1/Button-accent1.jsx
+++ client/src/components/buttons/Button-accent1/Button-accent1.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import styles from "./Button-accent1.module.css";
-import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+// import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 
 function ButtonAccent1({ text, route }) {
   return (
