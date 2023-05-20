38abe2846fc15b5f4cdd2ae8173e695c522c5c5f
martin marchessi <niitram.mm@gmail.com>
2023-05-20 00:36:01 -0300

    Arreglado de enrutamiento para F5 en deploy

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,14 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import { BrowserRouter } from "react-router-dom"
-import store from "./redux/store.js"
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import { HashRouter } from "react-router-dom";
+import store from "./redux/store.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App/>
-    </BrowserRouter>
-  </Provider>,
-)
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>
+);
