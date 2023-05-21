81a9eea1681be77bd14126f62838ab3f77251f90
martin marchessi <niitram.mm@gmail.com>
2023-05-20 22:06:48 -0300

    Creado de componente alert para mostrar cuando se agrega un producto al carrito

--- client/src/utils/getTopRatedProducts.js
+++ client/src/utils/getTopRatedProducts.js
@@ -2,7 +2,6 @@ function getTopRatedProducts(products) {
     let topRatedProducts = []
     const sortedProducts = products.sort((a, b) => b.rate - a.rate);
     if (sortedProducts.length >= 5) {
-        console.log(sortedProducts);
         topRatedProducts = sortedProducts.slice(0, 5);
     } else {
         topRatedProducts = sortedProducts
