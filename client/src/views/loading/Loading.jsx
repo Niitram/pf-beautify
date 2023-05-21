9e408f69d366b28b0fe6f6ed2d34ba9ee926a6de
AnaGarofalo <anamariagarofalo95@gmail.com>
2023-05-21 12:20:54 -0300

    added styles to loading

--- client/src/views/loading/Loading.jsx
+++ client/src/views/loading/Loading.jsx
@@ -1,5 +1,20 @@
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+import styles from "./Loading.module.css";
+
 const Loading = () => {
-  return <h1>Loading</h1>;
+  return (
+    <div className={styles.gif}>
+      <Box sx={{ display: "flex" }}>
+        <CircularProgress />
+      </Box>
+    </div>
+  );
 };
 
 export default Loading;
+
+// import CircularProgress from '@mui/material/CircularProgress';
+// import Box from '@mui/material/Box';
+
+// export default function CircularIndeterminate() {
