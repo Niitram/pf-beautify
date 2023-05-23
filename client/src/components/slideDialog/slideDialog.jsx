ff6c38a322f4b3651fbf6dd42f9db3b2f4357503
AnaGarofalo <anamariagarofalo95@gmail.com>
2023-05-23 19:08:16 -0300

    added a few styles to slide dialog

--- client/src/components/slideDialog/slideDialog.jsx
+++ client/src/components/slideDialog/slideDialog.jsx
@@ -2,10 +2,9 @@ import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
+// import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 import Slide from "@mui/material/Slide";
-// import React from "react";
 import { forwardRef } from "react";
 const Transition = forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
@@ -30,18 +29,30 @@ export default function AlertDialogSlide({
         aria-labelledby="alert-dialog-slide-title"
         aria-describedby="alert-dialog-slide-description"
       >
-        <DialogTitle id="alert-dialog-slide-title">{questionText}</DialogTitle>
-        <DialogContent>
-          {/* <DialogContentText id="alert-dialog-slide-description">
+        <DialogTitle
+          id="alert-dialog-slide-title"
+          style={{
+            color: "#3d3b54",
+            fontFamily: "Playfair Display",
+            fontWeight: "bold",
+          }}
+        >
+          {questionText}
+        </DialogTitle>
+        {/* <DialogContent >
+            <DialogContentText id="alert-dialog-slide-description">
             Let Google help apps determine location. This means sending anonymous location data to
             Google, even when no apps are running.
-          </DialogContentText> */}
-        </DialogContent>
+          </DialogContentText>
+          </DialogContent> */}
         <DialogActions>
-          <Button onClick={yesCallback} color="primary">
+          <Button
+            onClick={yesCallback}
+            style={{ backgroundColor: "#d14d72", color: "#fef2f4" }}
+          >
             Yes
           </Button>
-          <Button onClick={handleCloseDialog} color="primary">
+          <Button onClick={handleCloseDialog} style={{ color: "#d14d72" }}>
             No
           </Button>
         </DialogActions>
