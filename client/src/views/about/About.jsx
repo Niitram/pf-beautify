44ec53f9a83d877c3a3459f6699daa05001e88a8
lordeber15 <eberpalomino@hotmail.com>
2023-05-25 10:02:46 -0500

    About responsive

--- client/src/views/about/About.jsx
+++ client/src/views/about/About.jsx
@@ -4,12 +4,18 @@ function About() {
   return (
     <div className={styles.Container}>
       <div className={styles.QuienesSomos}>
-        <p>About us</p>
+        <h1>¿Quiénes somos?</h1>
         <p>
-          {" "}
-          Beautify is a platform that connects millions of clients, both men and
-          women, in order to provide the best attention in the distribution and
-          provision of services related to manicure and female personal care.
+          En <strong>Beautify</strong>, somos una empresa dedicada a ofrecer
+          productos y servicios de belleza de alta calidad. Nos enorgullece ser
+          un referente en la industria de los cosméticos, brindando a nuestros
+          clientes una amplia gama de productos y servicios que les ayudan a
+          realzar su belleza y confianza. Nuestra misión es proporcionar
+          soluciones de belleza innovadoras y efectivas que satisfagan las
+          necesidades individuales de cada cliente. Nos comprometemos a
+          seleccionar cuidadosamente los mejores productos del mercado,
+          trabajando con marcas reconocidas y formulaciones de calidad para
+          asegurar resultados excepcionales.
         </p>
       </div>
     </div>
