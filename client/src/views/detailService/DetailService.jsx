982eac4e2ad3cdbc8fae5515291e49eb6e2f6d5f
martin marchessi <niitram.mm@gmail.com>
2023-05-27 19:37:01 -0300

    view Detail

--- client/src/views/detailService/DetailService.jsx
+++ client/src/views/detailService/DetailService.jsx
@@ -5,6 +5,7 @@ import Reviews from "../../components/reviews/Reviews";
 import { getProfessionalById } from "../../request/professionals";
 import CardProfessional from "../../components/cardProfessional/CardProfessional";
 import styles from "./DetailService.module.css";
+import { Rating, Skeleton, Stack } from "@mui/material";
 
 function DetailService() {
   const [service, setService] = useState({});
@@ -19,10 +20,6 @@ function DetailService() {
           setProfessional(res.data);
         });
       });
-      /* getProfessionalById(3).then((res) => {
-        setProfessional(res.data);
-        console.log(res.data);
-      }); */
     } catch (error) {
       console.log(error.message);
     }
@@ -33,16 +30,43 @@ function DetailService() {
   }, []);
   return (
     <section className={styles.container}>
+      <h1>Service</h1>
       {service && (
-        <div>
-          <h1>{service.name}</h1>
-          <p>{service.description}</p>
-          <span>Price: {service.price}</span>
-          <span>Duration: {service.duration}</span>
-          <span>Rating: {service.rate}</span>
-          <img src={service.image} alt={service.name} />
+        <div className={styles.containerDetail}>
+          <img
+            className={styles.image}
+            src={service.image}
+            alt={service.name}
+          />
+          <div className={styles.containerInfo}>
+            <h2>{service.name}</h2>
+            <p className={styles.description}>{service.description}</p>
+            <div>
+              <h3>Duration</h3>
+              <span>{service.duration}</span>
+            </div>
+            <div>
+              <h3>Price</h3>
+              <span>${service.price}</span>
+            </div>
+            <div>
+              <h3 className={styles.rating}>Rating</h3>
+              {service.rate ? (
+                <Stack>
+                  <Rating
+                    value={service.rate < 1 ? 1 : service.rate}
+                    precision={0.5}
+                    readOnly
+                  />
+                </Stack>
+              ) : (
+                <Skeleton variant="rectangular" width={150} />
+              )}
+            </div>
+          </div>
         </div>
       )}
+      <h3>Professional</h3>
       <CardProfessional service={service} professional={professional} />
       <Reviews rate={service.rate} />
     </section>
