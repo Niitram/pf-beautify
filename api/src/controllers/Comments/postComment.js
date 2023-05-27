81d1151819f93eba5781df91c991f7bdbd864e3d
AnaGarofalo <anamariagarofalo95@gmail.com>
2023-05-27 10:33:11 -0300

    added productsComments routes and validatios

--- api/src/controllers/Comments/postComment.js
+++ api/src/controllers/Comments/postComment.js
@@ -1,15 +1,15 @@
-const { Comment} = require("../../db");
+const { Comment, Product } = require("../../db");
 
-const postComment = async (content, productId, clientId) => {
-    
- const newComment = await Comment.create(
-    {content}
- )
-   await newComment.setClient(clientId)
-   await newComment.setProduct(productId)
+const postComment = async (content, tittle, rating, productId, clientId) => {
+  const newComment = await Comment.create({ content, tittle, rating });
+  await newComment.setClient(clientId);
+  await newComment.setProduct(productId);
 
- return newComment
-}
+  const oldProduct = await Product.findByPk(productId);
+  const oldRates = oldProduct.arrayRates;
+  await oldProduct.update({ arrayRates: [...oldRates, rating] });
 
+  return newComment;
+};
 
-module.exports = postComment
+module.exports = postComment;
