a94ad3a6d5ab4c0718342afd0e7019d13693dcfe
Franco Rocamora <martin.rocamoraf@gmail.com>
2023-05-28 16:24:41 -0300

    mercadoPago services

--- api/src/handlers/mercadoPago-routes.js
+++ api/src/handlers/mercadoPago-routes.js
@@ -5,7 +5,7 @@ const { ACESS_TOKEN, BACK_ROUTE, ROUTE } = process.env;
 const approved = require("../controllers/MercadoPago/approved");
 const router = express();
 const mercadopago = require("mercadopago");
-
+const {createAppointment} = require("../controllers/Appointments/postAppointmentByClient")
 mercadopago.configure({
   access_token: `${ACESS_TOKEN}`,
 });
@@ -21,7 +21,6 @@ router.post("/create_preference", async (req, res) => {
     },
     auto_return: "approved",
   };
-  console.log(req.body)
   mercadopago.preferences
     .create(preference)
     .then(async function (response) {
@@ -30,7 +29,38 @@ router.post("/create_preference", async (req, res) => {
         clientMail: response.body.external_reference,
         returnUrl: req.headers.origin,
       });
-      console.log(response.body.id)
+      console.log(response.body.id);
+      res.json({
+        id: response.body.id,
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
+
+router.post("/service_preference", async (req, res) => {
+  let preference = {
+    external_reference: req.body.pop(),
+    items: req.body,
+    back_urls: {
+      success: `${BACK_ROUTE}/mercadopago/feedbackService`,
+      failure: `${BACK_ROUTE}/mercadopago/feedbackService`,
+      pending: `${BACK_ROUTE}/mercadopago/feedbackService`,
+    },
+    auto_return: "approved",
+  };
+  const data = JSON.stringify(req.body.pop())
+  mercadopago.preferences 
+    .create(preference)
+    .then(async function (response) {
+      await Purchase.create({
+        preferenceId: response.body.id,
+        appointmentData: data,
+        clientMail: response.body.external_reference,
+        returnUrl: req.headers.origin,
+      });
+      console.log(response.body.id);
       res.json({
         id: response.body.id,
       });
@@ -41,7 +71,6 @@ router.post("/create_preference", async (req, res) => {
 });
 
 router.get("/feedback", async (req, res) => {
-  console.log('llegué hasta acá')
   try {
     const email = req.query.external_reference;
     const toDelete = await Purchase.findAll({ where: { clientMail: email } });
@@ -52,7 +81,6 @@ router.get("/feedback", async (req, res) => {
     const products = toDelete[0].dataValues;
 
     if (req.query.status === "approved") {
-      console.log('de momento aprobado')
       const response = await approved(products.preferenceId, email);
       res.status(200).redirect(`${products.returnUrl}/#/purchaseSuccess`);
     } else {
@@ -61,6 +89,28 @@ router.get("/feedback", async (req, res) => {
         await each.destroy();
       });
       res.status(402).redirect(`${products.returnUrl}/#/purchaseError`);
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
+router.get("/feedbackService", async (req, res) => {
+  try {
+    const email = req.query.external_reference;
+    const toDelete = await Purchase.findAll({ where: { clientMail: email } });
+    for (let i = 0; i < toDelete.length -1 ; i++) {
+      toDelete[i].destroy();
+    }
+    const service = toDelete[0].dataValues;
+    console.log(service)
+    
+    const {profesionalId, clientId, serviceId, date, hour, paid} = JSON.parse(service.appointmentData)
+    console.log(date)
+    if (req.query.status === "approved") {
+      const response = await createAppointment(profesionalId, clientId, serviceId, date, hour, paid);
+      res.status(200).redirect(`${service.returnUrl}/#/purchaseSuccess`);
     }
   } catch (error) {
     res.status(500).json({ error: error.message });
