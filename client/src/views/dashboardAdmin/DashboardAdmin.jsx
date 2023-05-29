b9ce8be3613bc334b3f46b42ae974768aa64f3e9
martin marchessi <niitram.mm@gmail.com>
2023-05-29 11:41:13 -0300

    Fix de import con mayuscula

--- client/src/views/dashboardAdmin/DashboardAdmin.jsx
+++ client/src/views/dashboardAdmin/DashboardAdmin.jsx
@@ -1,16 +1,19 @@
-import {useState,useEffect} from "React"
+import { useState, useEffect } from "react";
 import styles from "./DashboardAdmin.module.css";
 import CardChart from "../../components/dashboard/CardChart";
-import RatingProducts from '../../components/dashboard/RatingProducts'
+import RatingProducts from "../../components/dashboard/RatingProducts";
 //import { SidebarAdmin } from "../../components/dashboard/SidebarAdmin";
 //import {ViewProfesional} from '../../components/dashboard/ViewProfesional'
-import {NavAdmin} from '../../components/dashboard/NavAdmin'
-import {Resumen} from '../../components/dashboard/Resumen'
-import FooterAll from '../../components/footerAll/FooterAll'
-import { getProductosMinMaxPrecio,getStaticsAppointments, getProductsByCategories}from '../../request/homeDashboard'
+import { NavAdmin } from "../../components/dashboard/NavAdmin";
+import { Resumen } from "../../components/dashboard/Resumen";
+import FooterAll from "../../components/footerAll/FooterAll";
+import {
+  getProductosMinMaxPrecio,
+  getStaticsAppointments,
+  getProductsByCategories,
+} from "../../request/homeDashboard";
 
-
-const DashboardAdmin = ()=>{
+const DashboardAdmin = () => {
   const [staticProducts, setStaticProducts] = useState({});
   const [staticAppointments, setStaticAppointments] = useState({});
   const [staticProductCategories, setStaticProductCategories] = useState({});
@@ -21,7 +24,7 @@ const DashboardAdmin = ()=>{
       setStaticProducts(resultado);
 
       const appointmentsResult = await getStaticsAppointments();
-      console.log(appointmentsResult); 
+      console.log(appointmentsResult);
       setStaticAppointments(appointmentsResult);
 
       const productCategoriesResult = await getProductsByCategories();
@@ -34,38 +37,51 @@ const DashboardAdmin = ()=>{
   const labelProducts = Object.keys(staticProducts);
   const dataProducts = Object.values(staticProducts);
 
-  const labelAppointments = staticAppointments ? Object.keys(staticAppointments) : [];
+  const labelAppointments = staticAppointments
+    ? Object.keys(staticAppointments)
+    : [];
 
   const dataAppointments = Object.values(staticAppointments);
 
   const labelProductCategories = Object.keys(staticProductCategories);
   const dataProductCategories = Object.values(staticProductCategories);
   return (
-<div className={styles.admin_container}>
- <NavAdmin/>
- <Resumen/>
- <div className={styles.rating}>
-  <div>
-  <RatingProducts/>
-  </div>
+    <div className={styles.admin_container}>
+      <NavAdmin />
+      <Resumen />
+      <div className={styles.rating}>
+        <div>
+          <RatingProducts />
+        </div>
 
- 
- <div className={styles.statics}> <CardChart title="Statics Products" labels={labelProducts} data={dataProducts} /> </div>
- <div className={styles.statics}>
-          <CardChart title="Appointment Statistics" labels={labelAppointments} data={dataAppointments} />
+        <div className={styles.statics}>
+          {" "}
+          <CardChart
+            title="Statics Products"
+            labels={labelProducts}
+            data={dataProducts}
+          />{" "}
         </div>
         <div className={styles.statics}>
-          <CardChart title="Product Categories" labels={labelProductCategories} data={dataProductCategories} />
+          <CardChart
+            title="Appointment Statistics"
+            labels={labelAppointments}
+            data={dataAppointments}
+          />
         </div>
- 
-
- </div>
- <br />
- <div className={styles.footeradmin}>
-  <FooterAll/>
-  </div>
-
-</div>
-  )
-}
+        <div className={styles.statics}>
+          <CardChart
+            title="Product Categories"
+            labels={labelProductCategories}
+            data={dataProductCategories}
+          />
+        </div>
+      </div>
+      <br />
+      <div className={styles.footeradmin}>
+        <FooterAll />
+      </div>
+    </div>
+  );
+};
 export default DashboardAdmin;
