2cd7277a9884221aa3b00b44fc6ab476e61cd4a7
martin marchessi <niitram.mm@gmail.com>
2023-05-30 11:46:52 -0300

    No se puede sacar un turno en un horario pasado

--- client/src/handlers/handlerClickCalendar.js
+++ client/src/handlers/handlerClickCalendar.js
@@ -32,6 +32,14 @@ const handlerClickCalendar = async (
   //Se hace la peticion de los horarios disponibles para ese dia
   try {
     const resp = await getAppointmentsHours(options.service, daySelected);
+    let hourNow = new Date().getHours();
+    //Si el horario es menor a la hora actual setea el estado a false
+    resp.data.forEach((hour) => {
+      if (Number(hour.hour.split(":")[0]) <= hourNow) hour.available = false;
+      return hour;
+
+    })
+    //Si hay horarios disponibles se setean en el state
     if (resp.data.length > 0) setAvailableSchedules(resp.data);
   } catch (error) {
     console.log(error.message);
