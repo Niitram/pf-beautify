7d3f56c0cf7c160657fbd2686ad27b076713b6c1
AnaGarofalo <anamariagarofalo95@gmail.com>
2023-05-31 09:49:23 -0300

    remove changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { HashRouter } from "react-router-dom";
 import store from "./redux/store.js";
+import "leaflet/dist/leaflet.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
