1e9840c0c871bc88174bcbf657ede2b46ec9e1d5
lordeber15 <eberpalomino@hotmail.com>
2023-05-31 12:11:58 -0500

    Cambios minimos

--- api/src/config-email/sendMail.js
+++ api/src/config-email/sendMail.js
@@ -1,22 +1,21 @@
+const nodemailer = require("nodemailer");
 
-const nodemailer = require('nodemailer');
+// Llamar a la función `mail` y pasar `mailOptions` como argumento
+const sendMail = async (toSend) => {
+  const transporter = nodemailer.createTransport({
+    service: "hotmail",
+    auth: {
+      user: "BeautifyStaff@hotmail.com",
+      pass: "beautify123",
+    },
+  });
 
-  // Llamar a la función `mail` y pasar `mailOptions` como argumento
-  const sendMail = async (toSend) => {
-    const transporter = nodemailer.createTransport({
-      service: 'hotmail',
-      auth: {
-        user: "BeautifyStaff@hotmail.com",
-        pass: "beautify123",
-      }
-    })
+  const info = await transporter.sendMail({
+    from: "Beautify <BeautifyStaff@hotmail.com>",
+    to: toSend.to,
+    subject: toSend.subject,
+    html: toSend.html,
+  });
+};
 
-    const info = await transporter.sendMail({
-        from: 'Beautify <BeautifyStaff@hotmail.com>', 
-        to: toSend.to,
-        subject: toSend.subject,
-        html: toSend.html
-    })
-  } 
-
-module.exports = sendMail
+module.exports = sendMail;
